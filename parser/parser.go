90a53e37bf5968c3b4f2ef8fe83a9c8e3a2a0a4f
Gihwan Kim <ghkim3221@gmail.com>
2023-02-09 17:31:13 +0900

    Parse integer literal

--- parser/parser.go
+++ parser/parser.go
@@ -26,6 +26,7 @@ package parser
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/ghkim3221/monkey/ast"
 	"github.com/ghkim3221/monkey/lexer"
@@ -48,6 +49,7 @@ func New(l *lexer.Lexer) *Parser {
 	}
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
+	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	// Read two tokens and set `curToken` and `peekToken`.
 	p.nextToken()
 	p.nextToken()
@@ -192,4 +194,18 @@ func (p *Parser) parseExpression(precedence int) ast.Expression {
 
 func (p *Parser) parseIdentifier() ast.Expression {
 	return &ast.Identifier{Token: p.curToken, Value: p.curToken.Literal}
+}
+
+func (p *Parser) parseIntegerLiteral() ast.Expression {
+	literal := &ast.IntegerLiteral{Token: p.curToken}
+
+	value, err := strconv.ParseInt(p.curToken.Literal, 0, 64)
+	if err != nil {
+		msg := fmt.Sprintf("could not parse %q as integer", p.curToken.Literal)
+		p.errors = append(p.errors, msg)
+		return nil
+	}
+
+	literal.Value = value
+	return literal
 }
