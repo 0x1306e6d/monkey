82d6f71b162ecbbd60cc778745a46d0c916904eb
Gihwan Kim <ghkim3221@gmail.com>
2023-04-14 13:30:34 +0900

    Improve REPL

--- repl/repl.go
+++ repl/repl.go
@@ -30,7 +30,7 @@ import (
 	"io"
 
 	"github.com/ghkim3221/monkey/lexer"
-	"github.com/ghkim3221/monkey/token"
+	"github.com/ghkim3221/monkey/parser"
 )
 
 func Start(in io.Reader, out io.Writer) {
@@ -45,9 +45,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Fprintf(out, "%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String())
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
